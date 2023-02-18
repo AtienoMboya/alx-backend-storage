9fe1d74a2ae6464eca88e7ad78ff35bc6d872a9a
AtienoMboya <sharonmboya894@gmail.com>
2023-02-18 17:00:31 +0000

    country

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,5 +5,5 @@ CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country CHAR(2) NOT NULL DEFULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
