92f486822399405c90cf647bbd255385e9fe0cbb
AtienoMboya <sharonmboya894@gmail.com>
2023-02-19 10:12:19 +0000

    trigger fix

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 --- creates a trigger that decreases the quantity of an item ater adding a new order
 
-CREATE TRIGGER order_decrease BEFORE INSERT ON orders
-FOR EACH ROW UPDATE items
-SET quantity = quatity - NEW.number
-WHERE name = NEW.item_name;
+CREATE TRIGGER items_subtract_quantity
+AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET items.quantity = items.quantity - NEW.number
+WHERE items.name = NEW.item_name;
